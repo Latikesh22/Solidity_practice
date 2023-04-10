40bc24bff057c7ba711e65470797873929e1eb74
^LaTiKeSh22^ <69394800+Latikesh22@users.noreply.github.com>
2023-04-10 17:03:03 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -2,35 +2,37 @@
 pragma solidity ^0.8.0;
 
 contract MyToken {
-    string tokenName = "GlobalOffensive";
-    string tokenAbbrv = "GO";
-    uint public totalSupply = 0;
-    address minter;
+    string public tokenName = "GlobalOffensive";
+    string public tokenAbbrv = "GO";
+    uint public totalSupply = 100;
+    address payable public minter;
 
     mapping(address => uint) public balances;
     mapping(address => bool) public isUser;
 
     constructor() {
-        minter = msg.sender;
+        minter = payable(msg.sender);
     }
 
-    function mint(address _address,uint _value) public {
-            require(msg.sender == minter);
-            totalSupply += _value;
-            balances[_address] += _value;
-        }
+    function addUser(address _address) public {
+        isUser[_address] = true;
+    }
 
-    function transfer(address _address,address _reciptent,uint _value) public {
-        require(minter == _address);
-        require(isUser[_reciptent] == true);
+    function mint(address _address, uint _value) public {
+        require(isUser[_address], "User does not exist");
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+
+    function transfer(address _recipient, uint _value) public payable {
+        require(isUser[_recipient], "Recipient does not exist");
+        balances[msg.sender] -= _value;
+        balances[_recipient] += _value;
+    }
+
+    function burn(address _address, uint _value) public {
+        require(balances[_address] >= _value, "Insufficient balance");
+        totalSupply -= _value;
         balances[_address] -= _value;
-        balances[_reciptent] += _value;
-    }
-
-    function burn(address _address,uint _value) public {
-        if (balances[_address] >= _value){
-            totalSupply -= _value;
-            balances[_address] -= _value;
-        }
     }
 }
